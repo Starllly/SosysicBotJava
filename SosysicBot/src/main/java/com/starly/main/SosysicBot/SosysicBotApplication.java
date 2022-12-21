581926181ae3f828f59ce7a31e23c22da3b2b46d
Starly <97104930+Starllly@users.noreply.github.com>
2022-12-21 17:43:11 +0300

    Initial commit

---
+++ SosysicBot/src/main/java/com/starly/main/SosysicBot/SosysicBotApplication.java
@@ -0,0 +1,13 @@
+package com.starly.main.SosysicBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosysicBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosysicBotApplication.class, args);
+	}
+
+}
---
+++ SosysicBot/src/test/java/com/starly/main/SosysicBot/SosysicBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starly.main.SosysicBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosysicBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
